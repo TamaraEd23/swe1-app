34f7457ebd1c0a55b73b0995fe6b149033301e6b
TamaraEd23 <tamaraed6@gmail.com>
2025-10-03 14:57:20 -0400

    adding to github repo

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include  # include lets you add app urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),  # this links the polls app urls
+]

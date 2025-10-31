0b56635ecbdfc27d4ae1447435e41048ec8f93c4
TamaraEd23 <tamaraed6@gmail.com>
2025-10-30 22:20:08 -0400

    Fix formatting with Black

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include  # include lets you add app urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),  # this links the polls app urls
+    path("admin/", admin.site.urls),
+    path("polls/", include("polls.urls")),  # this links the polls app urls
 ]
